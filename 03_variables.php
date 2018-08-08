4f8a93eea98dfe022dfa2ba10f2e08970d7e9606
Gilbert Rivera <GRivera@revspringinc.com>
2018-08-08 16:11:45 -0700

    variables in php

---
+++ 03_variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Varibles in PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+          <h1>Up & Running with PHP</h1>
+          <div class="code-content">
+            <?php
+
+                // This is where the variables are declared
+               $name = "Juan";
+               $favorite_number = 5;
+               $happy = true;
+
+               // This is where the values are outputted
+               echo $name;
+               echo "<hr>";
+               echo $favorite_number;
+               echo "<hr>";
+               echo $happy;
+            ?>
+          </div>
+    </div>
+  </body>
+</html>
