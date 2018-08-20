5df5846d823beab9565ec41b352aee16eff1404a
Gilbert Rivera <GRivera@revspringinc.com>
2018-08-20 15:53:08 -0700

    arrays

--- 05_arrays.php
+++ 05_arrays.php
@@ -2,13 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Arrays in PHP</title>
+    <title>Introduction to PHP</title>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
   </head>
   <body>
     <div class="container">
-          <h1>Arrays in PHP</h1>
+          <h1>Arrays</h1>
           <div class="code-content">
             <?php
                $fruits = array('apple', 'grapes', 'strawberries', 'bananas');
