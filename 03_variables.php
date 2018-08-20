cce3b1c704ece8e17fe66d2dd7d47ca6fef0304b
Gilbert Rivera <GRivera@revspringinc.com>
2018-08-20 15:47:50 -0700

    variables

--- 03_variables.php
+++ 03_variables.php
@@ -2,13 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Varibles in PHP</title>
+    <title>Introduction to PHP</title>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
   </head>
   <body>
     <div class="container">
-          <h1>Variables in PHP</h1>
+          <h1>Variables</h1>
           <div class="code-content">
             <?php
                 // This is where the variables are declared
