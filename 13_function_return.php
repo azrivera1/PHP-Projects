8e4f0768e5ec2ea55c792daef3010ea5cb3de1da
Gilbert Rivera <GRivera@revspringinc.com>
2018-08-20 16:16:55 -0700

    function return

--- 13_function_return.php
+++ 13_function_return.php
@@ -8,26 +8,22 @@
 </head>
   <body>
     <div class="container">
-        <h1>function return in PHP</h1>
+        <h1>Function Return</h1>
         <div class="code-content">
             <?php
               function shopping_cart_amount($amount) {
                   $amount = $amount * 0.10;
                   return $amount;
               }
-
               $shopping_cart = shopping_cart_amount("1000");
-
               function bank_balance($shopping_cart) {
                   $bank_balance = 2000;
-
                   if($shopping_cart > $bank_balance) {
                      echo "You don't have enough money to pay";
                   } else {
                      echo "The payment was submitted";
                   }
               }
-
               bank_balance($shopping_cart);
             ?>
          </div>
