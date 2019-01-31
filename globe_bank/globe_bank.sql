1cb07f97ec91cfdf15ee28beb5891c9b91f7e2fb
Gilbert Rivera Admin <a-grivera@corp.local>
2019-01-31 15:16:09 -0700

    database driven website using PHP and MySQL

---
+++ globe_bank/globe_bank.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 11, 2018 at 05:16 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `globe_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `menu_name` varchar(255) DEFAULT NULL,
+  `position` int(3) DEFAULT NULL,
+  `visible` tinyint(1) DEFAULT NULL,
+  `content` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `subject_id`, `menu_name`, `position`, `visible`, `content`) VALUES
+(1, 1, 'Globe Bank', 1, 1, NULL),
+(2, 1, 'History', 2, 1, NULL),
+(3, 1, 'Leadership', 3, 1, NULL),
+(4, 1, 'Contact Us', 4, 1, NULL),
+(5, 2, 'Banking', 1, 1, NULL),
+(6, 2, 'Credit Cards', 2, 1, NULL),
+(7, 2, 'Mortgages', 3, 1, NULL),
+(8, 3, 'Checking', 1, 1, NULL),
+(9, 3, 'Loans', 2, 1, NULL),
+(10, 3, 'Merchant Services', 3, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `menu_name` varchar(255) DEFAULT NULL,
+  `position` int(3) DEFAULT NULL,
+  `visible` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `menu_name`, `position`, `visible`) VALUES
+(1, 'test 2', 1, 1),
+(2, 'Consumer 2', 1, 0),
+(3, 'Small Company 5', 1, 0),
+(4, 'test 6', 1, 1),
+(5, 'Tango4', 1, 0),
+(6, 'junk 1', 1, 0),
+(7, 'testing', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_subject_id` (`subject_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ globe_bank/private/db_credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+  // defining constants
+  define("DB_SERVER", "Localhost");
+  define("DB_USER", "webuser");
+  define("DB_PASS", "password1");
+  define("DB_NAME", "globe_bank");
+
+
+?>
